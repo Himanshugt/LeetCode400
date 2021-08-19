f19289bafb2c8bb5a40eee57d63f54a3dec37c7d
Himanshugt <himan321gupta@gmail.com>
2021-08-20 01:37:01 +0530

    Remove_Element

--- 1-50/26_Remove_Duplicates_From_Sorted_Array.java
+++ 1-50/26_Remove_Duplicates_From_Sorted_Array.java
@@ -1,5 +1,15 @@
 // Problem link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
+/*
+ * The idea behind this is we maintain a pointer i and we start iterating the loop from the
+ * start and as soon as we get an element greater than the (i-1)th index element{Because we
+ * have initialized i as 1} we set this element as at the ith index element and move the i
+ * pointer ahead.
+ * So basically what we are doing is as we move ahead onto the array we eliminate the numbers
+ * which are equal to previous set number and only fix numbers into array that we need(We do this by the
+ * help of the i pointer).
+ */
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         
